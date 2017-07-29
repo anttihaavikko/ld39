e801d1bea0692b5f3310ffe6280673408f420b51
Antti Haavikko <antti.haavikko@focus.fi>
2017-07-29 13:32:57 +0300

    Add moving areas & more level design

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed = 1f;
+	public float offset = 0f;
+	public Vector3 direction = Vector3.zero;
+
+	private Rigidbody2D body;
+	private Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start () {
+		originalPosition = transform.localPosition;
+		body = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		transform.localPosition = originalPosition + direction * Mathf.Sin (Time.time * speed + offset * Mathf.PI);
+		body.MovePosition (originalPosition + direction * Mathf.Sin (Time.time * speed + offset * Mathf.PI));
+	}
+}
